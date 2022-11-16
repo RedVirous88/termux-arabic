6c7437b6c0aef6c9d8adc0917d78379d657de7e6
RedVirous88 <88195156+RedVirous88@users.noreply.github.com>
2022-11-16 20:33:43 +0200

    Add files via upload

---
+++ termux-ar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+echo """
+
+ ____          ___     ___
+|  _ \ ___  __| \ \   / (_)_ __ ___  _   _ ___
+| |_) / _ \/ _` |\ \ / /| | '__/ _ \| | | / __|
+|  _ <  __/ (_| | \ V / | | | | (_) | |_| \__ \
+|_| \_\___|\__,_|  \_/  |_|_|  \___/ \__,_|___/
+
+whatsapp: https://wa.me/+201024132476
+
+
+please wait ....
+
+
+"""
+sleep 20
+
+mv font.ttf /data/data/com.termux/files/home/.termux
+
+clear
+
+echo "all Done!"
